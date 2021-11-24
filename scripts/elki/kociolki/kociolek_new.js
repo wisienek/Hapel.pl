064a568394a235e4709d94b600c6a308ae83c450
Mwisni <40739949+wisienek@users.noreply.github.com>
2021-11-25 00:08:39 +0100

    ♻️ kociolki refactor p2

--- scripts/elki/kociolki/kociolek_new.js
+++ scripts/elki/kociolki/kociolek_new.js
@@ -6,12 +6,12 @@ var version = "22.11.2021-1";
 var dev = false;
 
 // immutable
-function baseKociolek(list=[], wait=0, temp=0, ignis=false, id=null) {
-    this.list = list;
-    this.wait = wait;
-    this.temp = temp;
-    this.ignis = ignis;
-    this.id = id;
+function baseKociolek({ list, wait, temp, ignis, id }) {
+    this.list = list || [];
+    this.wait = wait || 0;
+    this.temp = temp || 0;
+    this.ignis = ignis || false;
+    this.id = id || null;
 
     this.toJson = function() {
         return {
@@ -70,7 +70,7 @@ function init(e){
         var change;
 
         if( !elk.current ) { 
-            elk.current = new baseKociolek(null, null, null, null, "Główny" );
+            elk.current = new baseKociolek({ id: "Główny" });
             change = true; 
         }
 
@@ -156,7 +156,7 @@ new Gui("continueBrewing", function(e) {
 }, null, "start");
 
 // gui3
-new Gui("eliksirCreatorMenu", function(e) {
+new Gui("eliksirCreatorMenu", function(e, current, show) {
     // player, current, show
     var gui = baseGui();
 
@@ -170,7 +170,7 @@ new Gui("eliksirCreatorMenu", function(e) {
 
     var tmp = KOCIOLEK.getStoreddata().get('elki'); 
     tmp = JSON.parse(tmp) || {};
-    if(!current){
+    if( !current ){
         if( tmp.current && tmp.current.list && tmp.current.list.length > 0 ){
             if( !show )
                 e.player.message("[§cEliksiry§f] §7Kończysz tworzyć eliksir!");
@@ -186,29 +186,318 @@ new Gui("eliksirCreatorMenu", function(e) {
     e.player.showCustomGui(gui);
 }, null, "start");
 
+// gui 6
+new Gui("basicInputs", function(e) {
+    var gui = baseGui(true);
+
+    gui.addLabel(id("l-kociolek"), "§1Kociołek", 115, 0, 80, 20); // 99
+
+    gui.addTexturedButton(id("b-addWater"), "§7Wlej wodę", 5, 20, 80, 15, buttonTexture); // 201
+    gui.addTextField(id("in-addWater"), 90, 20, 30, 15); // 101
+    gui.addLabel(id("l-addWater"), "§3ml", 122, 20, 15, 15); // 151
+
+    gui.addTexturedButton(id("b-addAlcohol"), "§7Wlej alkohol", 5, 40, 80, 15, buttonTexture); // 202
+    gui.addTextField(id("in-addAlcohol"), 90, 40, 30, 15); // 102
+    gui.addLabel(id("l-addAlcohol"), "§3ml", 122, 40, 15, 15); // 152
+
+    gui.addTexturedButton(id("b-increaseTemperature"), "§7Zwiększ temperaturę", 5, 60, 80, 15, buttonTexture); // 203
+    gui.addTextField(id("in-increaseTemperature"), 90, 60, 30, 15); // 104
+    gui.addLabel(id("l-increaseTemperature"), "§3°C", 122, 60, 15, 15);
+    
+    gui.addTexturedButton(id("b-lowerTemperature"), "§7Zmniejsz temperaturę", 5, 80, 80, 15, buttonTexture); // 204
+    gui.addTextField(d("in-lowerTemperature"), 90, 80, 30, 15);
+    gui.addLabel(id("l-lowerTemperature"), "§3°C", 122, 80, 15, 15);
+
+    var ndata = KOCIOLEK.getStoreddata();
+    var elki = ndata.get('elki');
+    elki = JSON.parse(elki) || {};
+
+    gui.addLabel(id("l-temperature"), "§3Aktualna temperatura: §b"+ elki.current.temp +" §3°C", 20, 100, 100, 20);
+
+    // ignis
+    // gui.addTexturedButton(925, "", 190, 170, 30, 30, "customnpcs:textures/gui/ikonki/elki.png").setTextureOffset(35, 0);
+
+    gui.addTexturedButton(id("b-ignisOn"), "§cRozpal ogień", 150, 20, 80, 15, buttonTexture); // 205
+    gui.addTexturedButton(id("b-ignisOff"), "§bZgaś ogień", 150, 40, 80, 15, buttonTexture); // 206
+
+    gui.addTextField(id("in-customAction"), 150, 70, 80, 15); // 211
+    gui.addTexturedButton(id("b-customAction"), "§dDodaj akcję wyżej", 150, 90, 80, 15, buttonTexture); // 212
+
+    gui.addLabel(id("l-turner"), "§1Mieszadło", 115, 130, 80, 20); // 999
+
+    gui.addTexturedButton(id("b-turnLeft"), "§7Mieszaj w lewo", 5, 150, 80, 15, buttonTexture);// 207
+    gui.addTextField(id("in-turnLeft"), 90, 150, 30, 15); // 107
+    gui.addLabel(id("l-turnLeft"), "§3razy", 122, 150, 20, 15); // 157
+
+    gui.addTexturedButton(id("b-turnRight"), "§7Mieszaj w prawo", 5, 170, 80, 15, buttonTexture);
+    gui.addTextField(id("in-turnRight"), 90, 170, 30, 15);
+    gui.addLabel(id("l-turnRight"), "§3razy", 122, 170, 20, 15);
 
 
+    gui.addLabel(id("l-timeKeeper"), "§1Czasomierz", 115, 200, 80, 20); // 998
+    gui.addTexturedButton(id("b-timeKeeper"), "§7Odczekaj chwilkę", 5, 220, 80, 15, buttonTexture); // 209
+    gui.addTextField(id("in-timeKeeper"), 90, 220, 30, 15); // 109
+    gui.addLabel(id("l-timeKeeper"), "§3Minut", 122, 220, 20, 15); //159
+
+    gui.addTexturedButton(id("b-goBack"), "§8Powrót", 170, 235, 80, 15, buttonTexture);
+
+    e.player.showCustomGui(gui);
+}, null, "eliksirCreatorMenu");
+
+// gui7
+new Gui("ingredientSelector", function(e, b, search) {
+    var gui = baseGui(true);
+
+    if( !b ) {
+        gui.addLabel(id("l-ingredientType"), "§1Wybierz typ składnika", 95, 0, 80, 20);
+
+        // 301-305
+        gui.addTexturedButton(id("b-ingredientType1"), "§7Nieorganiczne", 40, 80, 80, 15, buttonTexture);
+        gui.addTexturedButton(id("b-ingredientType2"), "§7Bazy wodne", 130, 80, 80, 15, buttonTexture);
+        gui.addTexturedButton(id("b-ingredientType3"), "§7Roślinne", 40, 110, 80, 15, buttonTexture);
+        gui.addTexturedButton(id("b-ingredientType4"), "§7Zwierzęce", 130, 110, 80, 15, buttonTexture);
+        gui.addTexturedButton(id("b-ingredientType5"), "§7Własny (z eq)", 90, 140, 80, 15, buttonTexture);
+
+        gui.addTexturedButton(id("b-goBack"), "§8Powrót", 170, 235, 80, 15, buttonTexture);
+    }
+    else {
+        gui.addLabel(id("l-info"), "§1Wybierz Ilość i Składnik", 95, 0, 120, 20); // 9
+        gui.addLabel(id("l-type"), "§5" + b, 190, 20, 80, 20); // 10
+        gui.addLabel(id("l-warning"), "§4§l[UWAGA]", 5, 5, 80, 30)
+            .setHoverText(["§r§4Wpisz najpierw wartość potem kliknij dwukrotnie na składnik!"]); //19
+
+        var skladniki = getSkladnik({ typ: b });
+        if( !skladnik || !skladnik.result || skladniki.error ){
+            return e.player.message("[§cDebugger§f] §7Error, napisz do administracji! -" + skladniki.error);
+        }
+        skladniki = skladniki.result;
+
+        var lista1=[];
+        for( var i=0; i < skladniki.length; i++ ) {
+            if( search && skladniki[i].nazwa.toLowerCase().indexOf(search.toLowerCase()) > -1 ){
+                if( skladniki[i].dostępny == true ){
+                    lista1.push(skladniki[i].nazwa+"  ["+skladniki[i].jednostka+"]");
+                }
+            }else if( !search ){
+                if( skladniki[i].dostępny == true ){
+                    lista1.push(skladniki[i].nazwa+"  ["+skladniki[i].jednostka+"]");
+                }
+            }
+        }
+
+        gui.addLabel(id("l-quantity"), "§3ilość:", 105, 20, 60, 20); // 29
+        gui.addTextField(id("in-quantity"), 125, 22, 30, 15); // 5
+        gui.addScroll(id("s-ingredient"), 8, 45, 240, 180, lista1); // 32
+
+        gui.addTextField(id("in-name"), 90, 235, 70, 15); // 7
+        gui.addTexturedButton(id("b-searchByName"), "§bSzukaj po nazwie", 5, 235, 80, 15, buttonTexture); // 999
+
+        gui.addTexturedButton(id("b-goBack"), "§8Powrót", 170, 235, 80, 15, buttonTexture);
+    }
+
+    e.player.showCustomGui(gui);
+}, null, "eliksirCreatorMenu");
+
+// gui 8
+new Gui("stepList", function(e) {
+    var gui = baseGui(true);
+
+    var tmp = KOCIOLEK.getStoreddata().get('elki'); 
+    tmp = JSON.parse(tmp) || {};
+    var lista = tmp.current.list || [];
+
+    var cena = tmp.current.price || 0;
+
+    gui.addLabel(id("l-info"), "§1Aktualna lista kroków", 85, 10, 100, 20); // 9
+    gui.addLabel(id("l-price"), "§eCena: " + cena, 5, 10, 50, 20); // 10
+    gui.addLabel(id("l-info1"), "§3[I]", 230, 20, 15, 15)
+        .setHoverText(["§7Informacja o zaznaczonym kroku"]); // 11
+    gui.addScroll(id("s-list"), 8, 45, 240, 180, lista); // 43
+
+    gui.addTexturedButton(id("b-saveRecipe"), "§8Zapisz przepis", 5, 235, 80, 15, buttonTexture)
+        .setHoverText(["§7Aby stworzyć przepis z aktualnego", "§7Musisz mieć §azeszyt §7na §ctoolbarze"]); // 12
+
+    gui.addTexturedButton(id("b-goBack"), "§8Powrót", 170, 235, 80, 15, buttonTexture); // 991
+
+    e.player.showCustomGui(gui);
+}, null, "eliksirCreatorMenu");
+
+// gui 9
+new Gui("modifyIngredient", function(e, skladnik) {
+    var gui = baseGui(true);
+    //{name:sel[0], ile: sel[1], jednostka: sel[2], akcje: []}
+
+    gui.addLabel(id("l-info"), "§1Modyfikacja składnika: §b"+ skladnik.name +" "+ skladnik.ile +" "+ skladnik.jednostka, 40, 10, 200, 20); // 9
+    gui.addLabel(id("l-lm"), "§4[LM]", 10, 10, 20, 20)
+        .setHoverText(skladnik.akcje); // 19
+
+    if( skladnik.typ == "eq" )
+        gui.addLabel(id("l-info1"), "§cSkładnik własny", 40, 30, 200, 20); // 29
+    
+
+    //1 rząd
+    gui.addTexturedButton(id("b-crush"), "§7Zmiażdż", 40, 80, 80, 15, buttonTexture); // 401
+    gui.addTexturedButton(id("b-cut"), "§7Potnij", 40, 100, 80, 15, buttonTexture); // 402
+    gui.addTexturedButton(id("b-snip"), "§7Natnij", 40, 120, 80, 15, buttonTexture); // 403
+    gui.addTexturedButton(id("b-tear"), "§7Porwij", 40, 140, 80, 15, buttonTexture); // 404
+    gui.addTexturedButton(id("b-rub"), "§7Zetrzyj", 40, 160, 80, 15, buttonTexture); // 409
+    gui.addTexturedButton(id("b-break"), "§7Połam", 40, 180, 80, 15, buttonTexture); // 412
+    //2 rząd
+    gui.addTexturedButton(id("b-crumble"), "§7Pokrusz", 130, 80, 80, 15, buttonTexture); // 405
+    gui.addTexturedButton(id("b-peel"), "§7Obierz", 130, 100, 80, 15, buttonTexture); // 406
+    gui.addTexturedButton(id("b-burn"), "§7Przypal", 130, 120, 80, 15, buttonTexture); // 407
+    gui.addTexturedButton(id("b-dry"), "§7Wysusz", 130, 140, 80, 15, buttonTexture); // 408
+    gui.addTexturedButton(id("b-grind"), "§7Zmiel", 130, 160, 80, 15, buttonTexture); // 411
+
+    gui.addTexturedButton(id("b-addIngredient"), "§aDodaj składnik", 5, 235, 80, 15, buttonTexture); // 410
+    gui.addTexturedButton(id("b-put&pull"), "§bZanurz i wyciągnij", 90, 235, 80, 15, buttonTexture); // 413
+
+
+    gui.addTexturedButton(id("b-goBack"), "§8Powrót", 175, 235, 80, 15, buttonTexture);
+
+    e.player.showCustomGui(gui);
+}, null, "ingredientSelector");
+
+// gui 10
+new Gui("eqIngredient", function(e) {
+    //dodawanie składniku z eq
+    var gui = baseGui(true);
+    gui.addLabel(id("l-info"), "§1Wybierz Ilość i Składnik", 95, 0, 130, 20); // 9
+    gui.addLabel(id("l-warning"), "§4§l[UWAGA]", 5, 5, 80, 30)
+        .setHoverText(["§4Wpisz najpierw wartość potem kliknij dwukrotnie na składnik!"]); // 19
+
+    var items = e.player.getInventory().getItems();
+    var skladniki = [];
+    for( var i = 0; i < items.length; i++ ) {
+        var lore = items[i].getLore();
+        if( lore.length >= 2 )
+            if( lore[0].toLowerCase() == "§askładnik eliksiru" )
+                skladniki.push(items[i].getDisplayName()+" §7["+ lore[1] +"§7]");
+    }
+
+    gui.addLabel(id("l-quantity"), "§3ilość:", 105, 20, 60, 20); // 29
+    gui.addTextField(id("in-quantity1"), 125, 22, 30, 15); // 5
+    gui.addScroll(id("s-eqIngredient"), 5, 45, 245, 180, skladniki); // 42
+
+    gui.addTexturedButton(id("b-goBack"), "§8Powrót", 170, 235, 80, 15, buttonTexture);
+    e.player.showCustomGui(gui);
+}, null, "eliksirCreatorMenu");
+
+// gui 11
+new Gui("selectEliksirToSend", function(e, searched) {
+    var gui = baseGui();
+
+    gui.addLabel(id("l-info"), "§4Wpisz nazwę eliksiru", 90, 0, 100, 20); // 9
+
+    var data = searched ? getEliksir({ nazwal: searched }) : getEliksir();
+    if( !data || data.error )
+        return e.player.message("[§cDebugger§f] §7Error, pisz do administracji! "+ error);
+    
+    data = data.result;
+    var lista = [];
+
+    for( var i=0; i < data.length; i++ )
+        lista.push(data[i].nazwa);
+    
+
+    gui.addScroll(id("s-eliksirs"), 10, 15, 240, 210, lista); // 52
+
+    gui.addTextField(id("in-searchByName"), 90, 235, 70, 15); // 7
+    gui.addTexturedButton(id("b-searchByName"), "§bSzukaj po nazwie", 5, 235, 80, 15, buttonTexture); // 998
+
+    gui.addTexturedButton(id("b-goBack"), "§8Powrót", 170, 235, 80, 15, buttonTexture);
+
+    e.player.showCustomGui(gui);
+}, null, "eliksirCreatorMenu");
+
+// gui 12
+new Gui("eliksirInocreation", function(e) {
+    var gui = baseGui();
+
+    gui.addLabel(id("l-info"), "§9Wybierz eliksir do inokreacji", 70, 0, 160, 20); // 9
+
+    var items = e.player.getInventory().getItems();
+    var eliksiry = getMysteriousElixirs( items )
+
+    gui.addScroll(id("s-inocreation"), 10, 15, 240, 210, eliksiry); // 62
+    gui.addTexturedButton(id("b-goBack"), "§4Menu główne", 90, 230, 80, 15, buttonTexture); // 31
+
+    e.player.showCustomGui(gui);
+}, null, "start");
+
+// gui 13
+new Gui("labelMaker", function(e, eliksir) {
+    var gui = baseGui(true);
+
+    if(!eliksir){
+        gui.addLabel(id("l-info"), "§9Wybierz eliksir do Etykiety", 80, 0, 100, 20); // 9
+
+        // new function
+        var items = player.getInventory().getItems();
+        var eliksiry = getMysteriousElixirs( items );
+        
+        gui.addScroll(id("s-labelSelector"), 10, 15, 240, 210, eliksiry); // 63
+        gui.addTexturedButton(id("b-goBack"), "§4Menu główne", 90, 230, 80, 15, buttonTexture); // 31
+    }else{
+        gui.addLabel(id("l-info"), "§9Wpisz etykietę", 95, 0, 100, 20); // 9
+        gui.addLabel(id("l-info1"), "§c[I]", 5, 10, 100, 20)
+            .setHoverText(["§7Aby dodać kolor:","§7wpisz & i opis koloru","§7Np. §a&a §7/ §6&6"]); // 10
+
+        gui.addLabel(id("l-eliksir"), eliksir, 250, 0, 100, 20); // 11
+
+        gui.addTextField(id("in-labelText"), 70, 80, 120, 20); // 19
+        gui.addTexturedButton(id("b-addLabel"), "§2Dodaj etykietę", 90, 110, 80, 15, buttonTexture); // 20
+
+        gui.addTexturedButton(id("b-removeLabel"), "§cUsuń etykietę", 5, 230, 80, 15, buttonTexture); // 32
+        gui.addTexturedButton(id("b-goBack"), "§4Menu główne", 170, 230, 80, 15, buttonTexture); // 31
+    }
+
+    e.player.showCustomGui(gui);
+}, null, "start");
+
+
+
+// handlers:
+function customGuiScroll(e){
+    try{
+        if(!KOCIOLEK)
+            throw "Nie znaleziono egzekującego bloku!";
+
+    }
+    catch(er){
+        print("Scroll error: "+er);
+    }
+}
+
+
+
+
+// exported from other file
 var buttonTexture = "customnpcs:textures/gui/pp_button.png";
 function baseGui(b){
     try{
-        var gui = API.createCustomGui(1, 256, 256, false);
+        var gui = API.createCustomGui(id("g-base"), 256, 256, false); // 1
 
-        var x = gui.addLabel(56, "§c[P]", 265, 0, 30, 30);
+        var x = gui.addLabel(id("l-recipeInfo"), "§c[P]", 265, 0, 30, 30); // 56
 
         if(!KOCIOLEK)
             throw "Brak KOCIOLEK!";
 
         var sdata = KOCIOLEK.getStoreddata();
-        var elki = JSON.parse(sdata.get("elki"));
+        var elki = JSON.parse( sdata.get("elki") );
 
         b ? gui.setBackgroundTexture("customnpcs:textures/gui/elki_e.png") : gui.setBackgroundTexture("customnpcs:textures/gui/elki.png");
         
-        gui.addTexturedButton(55, "", 256, 30, 30, 30, "customnpcs:textures/gui/ikonki/elki.png").setHoverText(["§7Przełącz kociołek", "§7Aktualny kociołek: §a"+ (elki.id || "Główny"), "§7" + (sdata.get("kociolek") || "Kociołek cynowy, Rozmiar 2") ]);
-        if ( elki.side && elki.side.list.length > 0 ) { 
-            gui.addButton(57, "§a[+]", 290, 40, 20, 20).setHoverText(["§7Dodaj drugi kociołek", "§7Do aktualnego"]); 
-        }
+        gui.addTexturedButton(id("b-switchCrucible"), "", 256, 30, 30, 30, "customnpcs:textures/gui/ikonki/elki.png")
+            .setHoverText(["§7Przełącz kociołek", "§7Aktualny kociołek: §a"+ (elki.id || "Główny"), "§7" + (sdata.get("kociolek") || "Kociołek cynowy, Rozmiar 2") ]); // 55
+        
+        if ( elki.side && elki.side.list.length > 0 ) 
+            gui.addButton(id("b-mergeCrucibles"), "§a[+]", 290, 40, 20, 20)
+                .setHoverText(["§7Dodaj drugi kociołek", "§7Do aktualnego"]); // 57
+        
     
         if( sdata.get("przepis") ) { 
+
             // todo: CHANGE HOVER TEXT TO [1], [2], [3], [4], [5]... 20 lines max.
             x.setHoverText([sdata.get("przepis")]);
         } else x.setHoverText(["§7Przepis..."]);
@@ -216,9 +505,11 @@ function baseGui(b){
 
         if( elki.current && elki.current.wait > 0 ){
             if( elki.current.wait > Date.now() ){
-                var data = new Date(elki.current.wait);
-                var waitTo = "§c"+ data.getHours()+"§f:§a"+data.getMinutes()+"§f;§b"+data.getSeconds()+" §f(§d"+data.getDate()+"§f.§d"+(data.getMonth()+1)+"§f.§d"+data.getFullYear()+"§f)";
-                gui.addTexturedRect(954, "customnpcs:textures/gui/ikonki/elki.png", 256, 70, 30, 30, 64, 0).setHoverText(["§aZaczekaj do:", waitTo, "§0.", "§cHH§f:§aMM§f;§bSS §f(§dDD§f.§dMM§f.§dRRRR§f)"]);
+                var data = new Date( elki.current.wait );
+                var waitTo = "§c"+ data.getHours() +"§f:§a"+ data.getMinutes() +"§f;§b"+ data.getSeconds() +" §f(§d"+ data.getDate() +"§f.§d"+ (data.getMonth()+1) +"§f.§d"+ data.getFullYear() +"§f)";
+                
+                gui.addTexturedRect(id("l-waitUntil"), "customnpcs:textures/gui/ikonki/elki.png", 256, 70, 30, 30, 64, 0)
+                    .setHoverText(["§aZaczekaj do:", waitTo, "§0.", "§cHH§f:§aMM§f;§bSS §f(§dDD§f.§dMM§f.§dRRRR§f)"]); // 954
             }
         }
 
@@ -229,14 +520,15 @@ function baseGui(b){
     }
 }
 
-// exported from other file
 var _GUI_IDS = {};
 var _GUI = {};
 function id(name) {
     return _GUI_IDS[name] || ( _GUI_IDS[name] = Object.keys(_GUI_IDS).length + 1 );
 }
-function guiM(obj) {
-    return _GUI[obj.name] || ( _GUI[obj.name] = obj );
+function getGui( guiName ) {
+    if( !guiName || !_GUI[guiName] ) return { name: "", show: function() {}, next: null, prev: null };
+
+    return _GUI[guiName]
 }
 function Gui(_name, _show, _next, _prev){
     this.name = _name;
@@ -248,4 +540,20 @@ function Gui(_name, _show, _next, _prev){
     this.showPrev = _GUI[this.prev] ? _GUI[this.prev].show : null; 
 
     return _GUI[this.name] = this;
+}
+
+function getMysteriousElixirs( items ) {
+    var eliksiry = [];
+
+    if( !items || typeof items != "object" || items.length == 0 ) return eliksiry;
+
+    for( var i = 0; i < items.length; i++ ){
+        if( items[i].getDisplayName() == "§eTajemniczy Eliksir" ){
+            var lore = items[i].getLore();
+            if( lore.length >= 3 )
+                eliksiry.push(lore[0]+" §7§l/§r "+ lore[1]+" §7§l/§r "+lore[2]);
+        }
+    }
+
+    return eliksiry;
 }
